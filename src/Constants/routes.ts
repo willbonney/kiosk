d59c9b90575ed8232d3b138d43be76b93a83dc47
William Bonney <wbonney@ipdanalytics.com>
2020-06-19 18:26:48 -0400

    shareable link

---
+++ src/Constants/routes.ts
@@ -0,0 +1,9 @@
+export const SIGNUP: string = "/signup";
+export const LOGIN: string = "/login";
+export const LANDING: string = "/";
+export const HOME: string = "/home";
+export const PROFILE: string = "/profile";
+export const FRIENDS: string = "/friends";
+export const ACTIVITY: string = "/activity";
+export const PUBLIC_ACTIVITY: string = "/activity/:uid";
+export const PASSWORD_FORGET: string = "/password_forget";
